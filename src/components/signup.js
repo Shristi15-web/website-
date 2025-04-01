a7bbd0fb03d75b9c4ffbdfa3a3a2284ce369fa8c
Shristi15-web <11d27shristi@gmail.com>
2025-04-01 18:55:57 +0530

    Delete src directory

--- src/components/signup.js
+++
@@ -1,6 +0,0 @@
-<>
-  <meta charSet="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Sign Up Page</title>
-  <h1>Welcome to the Sign Up Page!</h1>
-</>
